dc3c833439c9853b3e72eaea27d8099e9ab89a34
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-05-31 15:29:54 +0300

    added hook doChange, logout dynamic menu change

--- Front-End/Shop/src/app/core/header/header.component.ts
+++ Front-End/Shop/src/app/core/header/header.component.ts
@@ -1,29 +1,37 @@
-import { Component, HostListener, OnInit} from '@angular/core';
+import { Component, HostListener, OnInit, DoCheck} from '@angular/core';
 import { CartService } from '../service/cart.service';
+import { StorageService } from '../service/storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
 
   
 
   verticalPosition?: boolean = false;
   displayMenu?: boolean = false;
+  isLoggedIn?: string;
   cartCounter: number = 0;
   constructor(
-    private sCart: CartService
+    private sCart: CartService,
+    private sStorage: StorageService,
   ) { }
-
+    
+  ngDoCheck() {
+    this.isLoggedIn = this.sStorage.getStorage()!;
+  }
+  
   ngOnInit(): void {
-    this.changeCounter();
+    this.changeContent();
     this.cartCounter = this.sCart.getCounter();
+    this.isLoggedIn = this.sStorage.getStorage()!;
   }
 
   @HostListener('window:click')
-  changeCounter() {
+  changeContent() {
     this.cartCounter = this.sCart.getCounter();
   }
 
@@ -36,4 +44,9 @@ export class HeaderComponent implements OnInit {
   showMenu() {
     this.displayMenu == false ? this.displayMenu = true : this.displayMenu = false;
   }
+
+  onLogout() {
+    this.sStorage.clearStorage();
+  }
 }
+
