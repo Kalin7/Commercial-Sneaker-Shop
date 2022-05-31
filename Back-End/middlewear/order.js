506afbd62b776b54a8523c7bac74cb680049f531
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-05-31 10:15:53 +0300

    create middlewear cheks does client have active discount

--- Back-End/middlewear/order.js
+++ Back-End/middlewear/order.js
@@ -1,3 +1,21 @@
 const { Discount } = require('../mongoDb/models/discounts');
-const { Sneaker } = require('../mongoDb/models/sneakers');
 
+
+function isOrderHaveDiscount() {
+    return  async (req, res, next) => {
+        const email = req.body.email;
+        const clientCode = req.body.discount;
+        const discount = await Discount.findOne({ email: email})
+
+        if (discount && discount.code === clientCode) {
+            res.locals.discount = {status: true}
+        }else {
+            res.locals.discount = {status: false}
+        }
+
+        next();
+
+    }
+}
+
+module.exports = {isOrderHaveDiscount}
