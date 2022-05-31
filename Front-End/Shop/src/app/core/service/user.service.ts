54340e6bb9a1b9afecc9103282bf5818e580336a
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-05-31 13:39:28 +0300

    added new method- registerNewUser

--- Front-End/Shop/src/app/core/service/user.service.ts
+++ Front-End/Shop/src/app/core/service/user.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IUser } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  url: string = 'http://localhost:3000/user/order';
+  url: string = 'http://localhost:3000/user';
   constructor(
     private http: HttpClient
   ) { }
 
+  registerNewUser(user: IUser): Observable<string> {
+    return this.http.post<string>(`${this.url}/register`, user);
+  }
+
   registerClientDiscountRequest(email: string): Observable<any> {
-    console.log(email)
-    return this.http.post<any>(`${this.url}/discount-request`, {email: email});
+    return this.http.post<any>(`${this.url}/order/discount-request`, {email: email});
   }
 }
