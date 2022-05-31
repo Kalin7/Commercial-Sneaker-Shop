4f87242f490f51536522f332a0632f44880157fe
Kalin7 <103098899+Kalin7@users.noreply.github.com>
2022-05-31 08:44:16 +0300

    Update order.js

--- Back-End/middlewear/order.js
+++ Back-End/middlewear/order.js
@@ -1,18 +1,3 @@
 const { Discount } = require('../mongoDb/models/discounts');
 const { Sneaker } = require('../mongoDb/models/sneakers');
 
-function isOrderHaveDiscount() {
-    return  async (req, res, next) => {
-        const email = req.body.email;
-        const code = req.body.discount;
-        const prods = req.body.products;
-        const products = [];
-     
-        
-
-       
-        next();
-    }
-}
-
-module.exports = {isOrderHaveDiscount}
